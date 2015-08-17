082741d77df88af8472fa154525207deec35511d
Michael Colavita <colavitam@gmail.com>
2015-08-17 01:38:40 -0400

    Basic UI

--- lib/rails_admin_duration.rb
+++ lib/rails_admin_duration.rb
@@ -12,6 +12,26 @@ module RailsAdmin
       module Types
         class Duration < RailsAdmin::Config::Fields::Base
           RailsAdmin::Config::Fields::Types::register(self)
+
+          register_instance_option :partial do
+            :form_duration
+          end
+
+          def days
+            value / 1.day
+          end
+
+          def hours
+            (value % 1.day) / 1.hour
+          end
+
+          def minutes
+            (value % 1.hour) / 1.minute
+          end
+
+          def seconds
+            (value % 1.minute) / 1.second
+          end
         end
       end
     end
@@ -19,8 +39,8 @@ module RailsAdmin
 end
 
 RailsAdmin::Config::Fields.register_factory do |parent, properties, fields|
-  if properties[:name] == :duration
-    fields << RailsAdmin::Config::Fields::Types::Duration.new(parent, properties[:name], properties)
+  if properties.name == :duration
+    fields << RailsAdmin::Config::Fields::Types::Duration.new(parent, properties.name, properties)
     true
   else
     false
--- lib/rails_admin_duration/engine.rb
+++ lib/rails_admin_duration/engine.rb
@@ -1,4 +1,9 @@
 module RailsAdminDuration
   class Engine < ::Rails::Engine
+    initializer "RailsAdminDuration precompile hook", group: :all do |app|
+      app.config.assets.precompile += [
+        'rails_admin/rails_admin_duration.css'
+      ]
+    end
   end
 end
